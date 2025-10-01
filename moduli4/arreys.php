06511a6d02a922c026048df95ad23390d8a3a7fd
anuari123 <anuarkiqmari1@gmail.com>
2025-10-01 20:28:37 +0200

    scope variables and arrays

---
+++ moduli4/arreys.php
@@ -0,0 +1,28 @@
+<?php
+/*$sports = ['Football', 'Basketball','Voleyball'];
+echo $sports[2];
+"<br>";
+
+echo end($sports);
+"<br>";
+echo count($sports);
+"<br>" ;
+
+$sports = arrey['Football', 'Basketball','Voleyball'];
+for($i = 0; $i < 3; $i++){
+    echo $sports[$i], "\n";
+}
+$sports = arrey['Football', 'Basketball','Voleyball','Golf'];
+array_pop($sports);
+var_dump($sports);
+
+$sports = arrey['Football', 'Basketball','Voleyball','Golf'];
+array_unshift($sports,"Tennis");
+var_dump($sports);
+*/
+
+$sports = arrey['Football', 'Basketball','Voleyball','Golf'];
+array_shift($sports);
+var_dump($sports);
+
+?>
---
+++ moduli4/variables.php
@@ -0,0 +1,12 @@
+<?php
+ $x = 5;
+ function localvariable(){
+    $y = 10;
+     echo = $y;
+    
+ }
+localvariable();
+echo "\n, $x";
+
+"<br>"
+  ?>
